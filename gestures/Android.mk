c2d223f4a8fe697193310bffe2d7708bc36fbe90
lindwurm <lindwurm.q@gmail.com>
2017-10-09 16:30:24 +0900

    Revert "Remove CMSDK Dependency"
    This reverts commit 12d2c4a83622d4b1f300eff0b288c31020311196.

--- gestures/Android.mk
+++ gestures/Android.mk
@@ -9,7 +9,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v7-recyclerview \
     android-support-v7-preference \
     android-support-v7-appcompat \
-    android-support-v14-preference 
+    android-support-v14-preference \
+    org.cyanogenmod.platform.internal
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay \
     --extra-packages android.support.v7.preference:android.support.v14.preference:android.support.v17.preference:android.support.v7.appcompat:android.support.v7.recyclerview
